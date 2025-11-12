9a9d90206edd43619fc258dd69bb5e1fec5601b7
nbhara1637 <nbhara1637@rcs-k12.us>
2025-11-12 18:11:17 -0500

    change drive

--- SampleAutonomous.java
+++ SampleAutonomous.java
@@ -40,7 +40,7 @@ public class SampleAutonomous extends LinearOpMode
             // Note, this example takes more than 30 seconds to execute, so turn OFF the auto timer.
 
             // Drive a large rectangle, turning at each corner
-            robot.drive(  84, 0.60, 0.25);
+            robot.drive(  70, 0.60, 0.25);
             robot.turnTo(90, 0.45, 0.5);
             robot.drive(  72, 0.60, 0.25);
             robot.turnTo(180, 0.45, 0.5);
